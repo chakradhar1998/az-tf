3a78e1eb025cc8b466649fdb614cea0513542306
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2025-06-29 15:16:18 +0530

    sql server

--- main.tf
+++ main.tf
@@ -3,3 +3,12 @@ resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "westus2"
 }
+
+module "avm-res-sql-server" {
+  source  = "Azure/avm-res-sql-server/azurerm"
+  version = "0.1.5"
+  # insert the 3 required variables here
+  location = "westus2"
+  resource_group_name =rg.name
+  server_version = "12.0"
+}
