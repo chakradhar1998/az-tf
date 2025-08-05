52784368b7a14665d86435ad8b6be9e5d1264675
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2025-08-05 16:48:20 +0530

    Add new resource group aztfresourceaugust-2025

--- main.tf
+++ main.tf
@@ -4,3 +4,8 @@ resource "azurerm_resource_group" "rg" {
   location = "westus2"
 }
 
+resource "azurerm_resource_group" "rg_august" {
+  name     = "aztfresourceaugust-2025"
+  location = "westus2"
+}
+
