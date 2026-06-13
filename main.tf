ea44419ab019b15886c64430934e286dbec3d499
chakradhar1998 <47327087+chakradhar1998@users.noreply.github.com>
2026-06-13 20:21:00 +0530

    Add virtual network resource to main.tf
    Added a new virtual network resource to the Terraform configuration.

--- main.tf
+++ main.tf
@@ -4,10 +4,13 @@ resource "azurerm_resource_group" "rg" {
   location = "westus2"
 }
 
-/*
-resource "azurerm_resource_group" "rg_august" {
-  name     = "aztfresourceaugust-2025"
-  location = "westus2"
-}
+
 
 */
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
