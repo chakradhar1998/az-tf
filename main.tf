98b40b971bd9697dd0cc7dfe28352eadc9c7014b
chakradhar1998 <47327087+chakradhar1998@users.noreply.github.com>
2026-06-13 20:09:40 +0530

    Rename resource group to 'learnTF'

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
-/*
+
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
+  name     = "learnTF"
   location = "westus2"
 }
 
+/*
 resource "azurerm_resource_group" "rg_august" {
   name     = "aztfresourceaugust-2025"
   location = "westus2"
