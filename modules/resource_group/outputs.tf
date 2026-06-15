460226c5485d363c1375aa96181f885e53e33204
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2026-06-15 14:52:42 +0530

    output

--- modules/resource_group/outputs.tf
+++ modules/resource_group/outputs.tf
@@ -1,3 +1,11 @@
 output "resource_group_id" {
   value = azurerm_resource_group.this.id
 }
+
+output "name" {
+  value = azurerm_resource_group.this.name
+}
+
+output "location" {
+  value = azurerm_resource_group.this.location
+}
