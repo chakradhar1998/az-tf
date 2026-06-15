6c66493913650f2e2896091b458ae0b865b4408c
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2026-06-15 13:54:36 +0530

    rg

--- main.tf
+++ main.tf
@@ -4,23 +4,3 @@ module "resource_group" {
   location = "westus2"
 }
 
-
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = "westus2"
-
-  tags = {
-    Environment = "Terraform Getting Started"
-    Team = "DevOps"
-  }
-
-}
-
-
-# Create a virtual network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "myTFVnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
