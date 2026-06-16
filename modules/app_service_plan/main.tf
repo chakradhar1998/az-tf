03823c5e20f1d86b69ba854ffd2546b684e23be7
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2026-06-16 17:13:46 +0530

    tags

--- modules/app_service_plan/main.tf
+++ modules/app_service_plan/main.tf
@@ -1,21 +1,11 @@
-/*
-resource "azurerm_app_service_plan" "this" {
-  name                = var.name
-  location            = var.location
-  resource_group_name = var.rgname
-  kind                = "Linux"
-  reserved            = true
-
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
-}
-*/
 resource "azurerm_service_plan" "this" {
   name                = var.name
   resource_group_name = var.rgname
   location            = var.location
   os_type             = "Linux"
   sku_name            = "F1"
+
+  tags = {
+    managedby = "Terraform"
+  }
 }
