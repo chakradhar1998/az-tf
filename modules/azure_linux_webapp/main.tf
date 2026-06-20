70f8b6906e10590ef59fdb121294672e33639021
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2026-06-20 18:50:46 +0530

    webapp

--- modules/azure_linux_webapp/main.tf
+++ modules/azure_linux_webapp/main.tf
@@ -5,19 +5,22 @@ resource "azurerm_linux_web_app" "this" {
   service_plan_id     = var.service_plan_id
   tags = var.tags
 
-  site_config = var.site_config
-  /*
   site_config {
-    always_on = false
-    ip_restriction_default_action = "Deny"
-    ip_restriction {
-        name = "allowhomeip"
-        action = "Allow"
-        ip_address = "183.83.229.0/24"
-        priority = 150
+    always_on                     = var.site_config.always_on
+    ip_restriction_default_action = var.site_config.ip_restriction_default_action
+
+    dynamic "ip_restriction" {
+      for_each = [var.site_config.ip_restriction]
+      content {
+        name       = ip_restriction.value.name
+        action     = ip_restriction.value.action
+        ip_address = ip_restriction.value.ip_address
+        priority   = ip_restriction.value.priority
+      }
     }
+
     application_stack {
-        node_version = "24-lts"
+      node_version = var.site_config.application_stack.node_version
     }
-  }*/
+  }
 } 
--- terraform.auto.tfvars
+++ terraform.auto.tfvars
@@ -12,16 +12,16 @@ os_type = "Linux"
 
 webapp_name = "chakraapp"
 
-  site_config {
-    always_on = false
-    ip_restriction_default_action = "Deny"
-    ip_restriction {
-        name = "allowhomeip"
-        action = "Allow"
-        ip_address = "183.83.229.0/24"
-        priority = 150
-    }
-    application_stack {
-        node_version = "24-lts"
-    }
+site_config = {
+  always_on = false
+  ip_restriction_default_action = "Deny"
+  ip_restriction = {
+    name       = "allowhomeip"
+    action     = "Allow"
+    ip_address = "183.83.229.0/24"
+    priority   = 150
   }
+  application_stack = {
+    node_version = "24-lts"
+  }
+}
